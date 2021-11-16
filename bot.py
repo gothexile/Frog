f5f9e8bc6b4f349fcf49a37535ec569bb554e26e
gothexile <86521553+gothexile@users.noreply.github.com>
2021-11-16 15:28:35 +0000

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,18 @@
+from os import closerange
+import discord
+from discord.ext import commands
+
+from image_cog import image_cog
+
+Bot = commands.Bot(command_prefix='/')
+
+Bot.add_cog(image_cog(Bot))
+
+@Bot.command()
+async def echo(ctx, *args):
+    m_args = " ".join(args)
+    await ctx.send(m_args)
+token = ""
+with open("token.txt") as file:
+    token = file.read()
+Bot.run(token)
---
+++ image_cog.py
@@ -0,0 +1,66 @@
+import discord
+from discord.ext import commands 
+
+import os, shutil
+from google_images_download import google_images_download
+import random
+
+class image_cog(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.download_folder = 'downloads'
+        
+        self.keywords = 'frog' 
+        
+        self.response = google_images_download.googleimagesdownload()
+
+        self.arguments = {
+            "keywords": self.keywords,
+            "limit": 20,
+            "size": "medium",
+            "no_directory": True
+        }
+
+        self.image_names = []
+        self.update_images()
+       
+
+    def update_images(self):
+        for filename in os.listdir(self.download_folder):
+            self.image_names.append(os.path.join(self.download_folder, filename))
+
+    def clear_folder(self):
+        for filename in os.listdir(self.download_folder):
+            file_path = os.path.join(self.download_folder, filename)
+            try:
+                if os.path.isfile(file_path) or os.path.islink(file_path):
+                    os.unlink(file_path)
+                elif os.path.isdir(file_path):
+                    shutil.rmtree(file_path)
+            except Exception as e:
+                print("Failed to delete %s. reason %s" % (file_path, e))
+    
+    @commands.command()
+    async def get(self, ctx):
+        images_size = len(self.image_names)
+        random_image = random.randint(0, images_size)
+        img_path = self.image_names[random_image]
+
+        await ctx.send(file=discord.File(img_path))
+    
+    @commands.command()
+    async def search(self, ctx, *args):
+        self.clear_folder()
+
+        search_args = " ".join(args)
+        self.arguments['keywords'] = search_args
+        
+        self.response.download(self.arguments)
+
+        self.update_images()
+
+
+
+
+        
+     
